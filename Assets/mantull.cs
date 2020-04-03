01fa7b2d92dd09a756d8ff77cf4d4bda4533836f
annida <18523112@students.uii.ac.id>
2020-04-03 15:56:34 +0700

    Membuat animasi

--- Assets/mantull.cs
+++ Assets/mantull.cs
@@ -9,15 +9,22 @@ public class mantull : MonoBehaviour
 
 	public int speed= 30;
     public Rigidbody2D sesuatu;
+
+    public Animator animtr;
     void Start()
     {
     	sesuatu.velocity = new Vector2(-1, -1)*speed;
-        
+        animtr.SetBool("IsMove", true);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if(sesuatu.velocity.x > 0){ //bola bergerak ke kanan
+            sesuatu.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        }else{
+            sesuatu.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        }
         
     }
     //Mengecek apakah ada tabrakan
@@ -28,9 +35,10 @@ public class mantull : MonoBehaviour
     }
     IEnumerator jeda(){
         sesuatu.velocity = Vector2.zero;
+        animtr.SetBool("IsMove", false);
         sesuatu.GetComponent<Transform>().position = Vector2.zero;//bola pindah ke tengah setelah menabrak dinding
         yield return new WaitForSeconds(1);
         sesuatu.velocity = new Vector2(-1, -1)*speed;
-        
+        animtr.SetBool("IsMove", true);
     }
 }
